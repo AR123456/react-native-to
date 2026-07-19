e25567417b350d340e6c3522adf77006f34295dd
AR123456 <coloradoz99@gmail.com>
2026-07-19 06:05:22 -0400

    flat list imports

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { View, FlatList, StyleSheet, Text, StatusBar } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
